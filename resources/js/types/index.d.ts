373ee6e9f0b04bb55df7e60f66f568a3f730f796
Eric Mai <eric.mai@my-digital-school.org>
2025-06-02 11:13:09 +0200

    change app icon + refactor Course type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -41,3 +41,14 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+interface Course {
+  id: number;
+  name: string;
+  students: Array<{
+    id: number;
+    name: string;
+    has_signed?: boolean;
+  }>;
+  icon: string;
+}
